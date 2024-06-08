ff365f90053de72b303b1c4fad1373a0dd3e185a
Christian <filetelopezchristian@gmail.com>
2024-06-08 14:31:27 -0600

    Created file :ghost:

---
+++ src/utils/validations.ts
@@ -0,0 +1,9 @@
+import { REGEX_EMAIL } from "../constants";
+
+export const isEmpty = (value: string) => {
+  return value === undefined || value === null || value === "";
+};
+
+export const isValidEmail = (email: string) => {
+  return REGEX_EMAIL.test(email);
+};
