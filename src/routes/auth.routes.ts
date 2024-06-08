5aa6ffb068a217666a75a0b101a0453861f33b58
Christian <filetelopezchristian@gmail.com>
2024-06-08 16:13:10 -0600

    Non-important changes :monkey:

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,5 +4,6 @@ import * as authController from "../controllers/auth.controllers";
 const router = Router();
 
 router.post("/register", authController.register);
+router.post("/login", authController.logIn);
 
 export default router;
