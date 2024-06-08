d15468a4a2de7c0ed34634f7ba219e8908448145
Christian <filetelopezchristian@gmail.com>
2024-06-08 14:31:43 -0600

    Created file :ghost:

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+export default app;
