7ecea0c296312206cace4ff58edcfb185108609b
Christian <filetelopezchristian@gmail.com>
2024-06-08 16:13:15 -0600

    Non-important changes :monkey:

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,16 +1,24 @@
 import prisma from "../database";
-import { encryptPassword } from "../utils/encryption";
-import { CreateUserDto } from "../dto/auth.dto";
+import { encryptPassword, decryptPassword } from "../utils/encryption";
+import { CreateUserDto, LoginUserDto } from "../dto/auth.dto";
+import { generateToken } from "../utils/token";
 
 export const register = async (user: CreateUserDto) => {
   try {
     const encryptedPassword = await encryptPassword(user.password);
 
-    await prisma.user.create({
+    await prisma.image.create({
       data: {
-        userName: user.userName,
-        email: user.email,
-        password: encryptedPassword,
+        user: {
+          create: {
+            email: user.email,
+            userName: user.userName,
+            password: encryptedPassword,
+          },
+        },
+      },
+      include: {
+        user: true,
       },
     });
 
@@ -22,6 +30,45 @@ export const register = async (user: CreateUserDto) => {
   } catch (error) {
     console.error(error);
     return { message: "Error registering user", status: 500, error: true };
+  }
+};
+
+export const logIn = async (user: LoginUserDto) => {
+  try {
+    const userFound = await prisma.user.findUnique({
+      where: {
+        email: user.email,
+      },
+      include: {
+        image: true,
+      },
+    });
+
+    if (!userFound)
+      return { message: "User not found", status: 404, error: true };
+
+    const passwordMatch = await decryptPassword(
+      user.password,
+      userFound.password
+    );
+
+    if (!passwordMatch)
+      return { message: "Password incorrect", status: 401, error: true };
+
+    const token = generateToken(userFound.id);
+
+    return {
+      message: "User logged in successfully",
+      status: 200,
+      error: false,
+      data: {
+        token,
+        user: userFound,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return { message: "Error validating user", status: 500, error: true };
   }
 };
 
