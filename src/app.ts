ae50ee23c1cd0dd95e43f4859eff3c6d990fb81a
Christian <filetelopezchristian@gmail.com>
2024-06-09 14:42:38 -0600

    Non-important changes :monkey:

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-import authRoutes from "./routes/auth.routes";
+import authRoutes from './routes/auth.routes'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
 
-app.use("/api/auth", authRoutes);
+app.use('/api/auth', authRoutes)
 
-export default app;
+export default app
