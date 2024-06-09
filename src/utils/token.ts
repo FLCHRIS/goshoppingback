426df44d473740b29742a5df1d9d445722571875
Christian <filetelopezchristian@gmail.com>
2024-06-09 14:42:26 -0600

    Non-important changes :monkey:

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,10 +1,10 @@
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../utils/environments";
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../utils/environments'
 
 export const generateToken = (id: number) => {
   const token = jwt.sign({ id }, JWT_SECRET as jwt.Secret, {
     expiresIn: 86400,
-  });
+  })
 
-  return token;
-};
+  return token
+}
