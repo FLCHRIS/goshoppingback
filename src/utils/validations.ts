8d384e0c0561e41b69d1d9d856c57e59c115d110
Christian <filetelopezchristian@gmail.com>
2024-06-09 16:38:23 -0600

    New method IS_NUMBER created

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,4 +1,4 @@
-import { REGEX_EMAIL } from '../constants'
+import { REGEX_EMAIL, NUMBER_REGEX } from '../constants'
 
 export const isEmpty = (value: string) => {
   return value === undefined || value === null || value === ''
@@ -6,4 +6,8 @@ export const isEmpty = (value: string) => {
 
 export const isValidEmail = (email: string) => {
   return REGEX_EMAIL.test(email)
+}
+
+export const isNumber = (value: string) => {
+  return NUMBER_REGEX.test(value)
 }
