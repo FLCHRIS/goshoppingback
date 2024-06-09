fd631e1115539bc8a22a5b379594b05b0495d508
Christian <filetelopezchristian@gmail.com>
2024-06-09 16:34:19 -0600

    Router USERS initialized

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 
 import authRoutes from './routes/auth.routes'
+import userRoutes from './routes/user.routes'
 
 dotenv.config()
 
@@ -14,5 +15,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
 
 export default app
