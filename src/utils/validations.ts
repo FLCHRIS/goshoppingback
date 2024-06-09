b75b9fc8be5e0a7d81d60b1545b303d99e90c1ff
Christian <filetelopezchristian@gmail.com>
2024-06-09 14:42:33 -0600

    Non-important changes :monkey:

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,9 +1,9 @@
-import { REGEX_EMAIL } from "../constants";
+import { REGEX_EMAIL } from '../constants'
 
 export const isEmpty = (value: string) => {
-  return value === undefined || value === null || value === "";
-};
+  return value === undefined || value === null || value === ''
+}
 
 export const isValidEmail = (email: string) => {
-  return REGEX_EMAIL.test(email);
-};
+  return REGEX_EMAIL.test(email)
+}
