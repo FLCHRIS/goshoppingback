46f71ca28585a5341d32d8496e8975beb990ef8f
Christian <filetelopezchristian@gmail.com>
2024-06-09 14:43:39 -0600

    Non-important changes :monkey:

--- src/cloudinary/index.ts
+++ src/cloudinary/index.ts
@@ -1,19 +1,19 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary'
 import {
   CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET,
-} from "../utils/environments";
+} from '../utils/environments'
 
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
   api_key: CLOUDINARY_API_KEY,
   api_secret: CLOUDINARY_API_SECRET,
   secure: true,
-});
+})
 
 export const uploadImage = async (filePath: string, folder: string) => {
   return await cloudinary.uploader.upload(filePath, {
     folder: `goshopping/${folder}`,
-  });
-};
+  })
+}
