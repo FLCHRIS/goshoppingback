8c09cbe7134de1b99a4799e7ba81233f95c43b5f
Christian <filetelopezchristian@gmail.com>
2024-06-11 20:03:27 -0600

    New method EDIT_PHOTO created

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express'
+import fileUpload from 'express-fileupload'
+
 import { EditUserDto } from '../dto/user.dto'
 import { isNumber } from '../utils/validations'
 import * as userService from '../services/user.services'
@@ -12,6 +14,23 @@ export const editUser = async (req: Request, res: Response) => {
   const { error, message, status, data } = await userService.editUser(
     user,
     Number(id),
+  )
+
+  if (error) return res.status(status).json({ message })
+
+  return res.status(status).json({ message, data })
+}
+
+export const editPhoto = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const image = req.files?.image as fileUpload.UploadedFile
+
+  if (!isNumber(id)) return res.status(400).json({ message: 'Id is not valid' })
+  if (!image) return res.status(400).json({ message: 'Image is required' })
+
+  const { error, message, status, data } = await userService.editPhoto(
+    Number(id),
+    image.tempFilePath,
   )
 
   if (error) return res.status(status).json({ message })
