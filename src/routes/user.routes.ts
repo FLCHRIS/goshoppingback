ef218012014be44bc124e7f1b984c1258382e73b
Christian <filetelopezchristian@gmail.com>
2024-06-11 20:03:44 -0600

    New method EDIT_PHOTO created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
+import fileUpload from 'express-fileupload'
+
 import { verifyToken } from '../middlewares/verifyToken.middlewares'
 import * as userController from '../controllers/user.controllers'
 
 const router = Router()
 
 router.patch('/:id', verifyToken, userController.editUser)
+router.patch(
+  '/:id/photo',
+  verifyToken,
+  fileUpload({ useTempFiles: true, tempFileDir: './uploads' }),
+  userController.editPhoto,
+)
 
 export default router
