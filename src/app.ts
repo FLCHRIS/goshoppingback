e0d3d337389cb84747f07a639895f14d4c3521cc
Christian <filetelopezchristian@gmail.com>
2024-06-12 20:04:44 -0600

    New router created :monkey:

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 
 import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
+import productRoutes from './routes/product.routes'
 
 dotenv.config()
 
@@ -16,5 +17,6 @@ app.use(cors())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/products', productRoutes)
 
 export default app
