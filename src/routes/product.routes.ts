4469eed682c255f78ee2720d9b9914b867c1b17c
Christian <filetelopezchristian@gmail.com>
2024-06-13 20:51:08 -0600

    Created router CREATE_FILE

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express'
+import fileUpload from 'express-fileupload'
+
+import { verifyToken } from '../middlewares/verifyToken.middlewares'
+import * as productController from '../controllers/product.controllers'
 
 const router = Router()
+
+router.post(
+  '/:userId',
+  verifyToken,
+  fileUpload({ useTempFiles: true, tempFileDir: './uploads' }),
+  productController.createProduct,
+)
 
 export default router
