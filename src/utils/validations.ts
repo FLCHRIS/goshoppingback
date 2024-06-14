ce8d08dee5c0418a36634925171410df2493a7c6
Christian <filetelopezchristian@gmail.com>
2024-06-13 20:51:33 -0600

    Non-important changes :monkey:

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,6 +1,6 @@
 import { REGEX_EMAIL, NUMBER_REGEX } from '../constants'
 
-export const isEmpty = (value: string) => {
+export const isEmptyString = (value: string) => {
   return value === undefined || value === null || value === ''
 }
 
@@ -10,4 +10,12 @@ export const isValidEmail = (email: string) => {
 
 export const isNumber = (value: string) => {
   return NUMBER_REGEX.test(value)
+}
+
+export const isEmptyNumber = (value: number) => {
+  return value === undefined || value === null
+}
+
+export const isPositiveNumber = (value: number) => {
+  return value > 0
 }
