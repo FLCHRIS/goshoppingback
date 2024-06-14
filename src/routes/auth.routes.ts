c79292fd64ecc19d87aa66ee219266a947a5c0c1
Christian <filetelopezchristian@gmail.com>
2024-06-14 12:23:04 -0600

    New router LOG_OUT added

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import { verifyToken } from '../middlewares/verifyToken.middlewares'
 import * as authController from '../controllers/auth.controllers'
 
 const router = Router()
 
 router.post('/register', authController.register)
 router.post('/login', authController.logIn)
-router.post('/logout', authController.logOut)
+router.post('/logout', verifyToken, authController.logOut)
 
 export default router
