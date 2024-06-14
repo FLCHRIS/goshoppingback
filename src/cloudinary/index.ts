0ddad74425fe9bc194655accfbb92c8e6ca52e5f
Christian <filetelopezchristian@gmail.com>
2024-06-14 12:22:40 -0600

    Non-important changes :monkey:

--- src/cloudinary/index.ts
+++ src/cloudinary/index.ts
@@ -1,9 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary'
-import {
-  CLOUDINARY_CLOUD_NAME,
-  CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET,
-} from '../utils/environments'
+
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
 
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
