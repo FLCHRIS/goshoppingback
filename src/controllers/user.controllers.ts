bb9635bcb85e142138c5079465a5c9996f4e99f6
Christian <filetelopezchristian@gmail.com>
2024-07-12 17:05:12 -0600

    Non-important changes :monkey:

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -2,14 +2,13 @@ import { Request, Response } from 'express'
 import fileUpload from 'express-fileupload'
 
 import { EditUserDto } from '../dto/user.dto'
-import { isNumber } from '../utils/validations'
 import * as userService from '../services/user.services'
 
 export const editUser = async (req: Request, res: Response) => {
   const { id } = req.params
   const user = req.body as EditUserDto
 
-  if (!isNumber(id)) return res.status(400).json({ message: 'Id is not valid' })
+  if (!Number.isInteger(Number(id)) || Number(id) < 1) return res.status(400).json({ message: 'Id is not valid' })
 
   const { error, message, status, data } = await userService.editUser(
     user,
@@ -25,7 +24,7 @@ export const editPhoto = async (req: Request, res: Response) => {
   const { id } = req.params
   const image = req.files?.image as fileUpload.UploadedFile
 
-  if (!isNumber(id)) return res.status(400).json({ message: 'Id is not valid' })
+  if (!Number.isInteger(Number(id)) || Number(id) < 1) return res.status(400).json({ message: 'Id is not valid' })
   if (!image) return res.status(400).json({ message: 'Image is required' })
 
   const { error, message, status, data } = await userService.editPhoto(
@@ -41,7 +40,7 @@ export const editPhoto = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
   const { id } = req.params
 
-  if (!isNumber(id)) return res.status(400).json({ message: 'Id is not valid' })
+  if (!Number.isInteger(Number(id)) || Number(id) < 1) return res.status(400).json({ message: 'Id is not valid' })
 
   const { message, status } = await userService.deleteUser(Number(id))
 
